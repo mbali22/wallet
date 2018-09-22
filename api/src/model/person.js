9a6a475821ca0b4168dfcbb5b0d879a8167b2b11
mbali <mutturaj.bali@move.com>
2018-09-22 13:42:22 +0530

    person management

--- api/src/model/person.js
+++ api/src/model/person.js
@@ -16,8 +16,12 @@ class personRepo {
             resolve(person);
             let newPerson = this.getPutParamsForDb(person);
             docClient.put(newPerson, function(err, data) {
-                if (err) ppJson(err); // an error occurred
-                else ppJson(data); // successful response
+                if (err){
+                    reject(err);
+                }
+                else{
+                    
+                }
             });
         });
     }
@@ -35,6 +39,41 @@ class personRepo {
             Item:item,
             ConditionExpression: 'attribute_not_exists(belongsTo) AND attribute_not_exists(id)',           
             ReturnValues: 'NONE', // optional (NONE | ALL_OLD)
+        };
+        return params;
+    }
+
+    getParamsForDb(personId){
+        var params = {
+            TableName: tables["persons"],
+            KeyConditionExpression: '#whose = :value', 
+            ExpressionAttributeNames: {
+                '#whose': 'belongsTo'
+            },
+            ExpressionAttributeValues: { // a map of substitutions for all attribute values
+              ':value': personId
+            }
+        };
+        return params;
+    }
+
+    updateParamsForDb(person){
+        var params = {
+            TableName:  tables["persons"],
+            Key: { 
+                "belongsTo": person.belongsTo,
+                "id":person.id
+            },
+            UpdateExpression: 'SET attribute_name :value', 
+            ConditionExpression: 'attribute_exists(belongsTo) AND attribute_exists(id)', 
+            ExpressionAttributeNames: { 
+                '#lname': 'lname'
+            },
+            ExpressionAttributeValues: { // a map of substitutions for all attribute values
+                ":lname": "chalvadi",
+                "Mobile":"9741732306"
+            },
+            ReturnValues: 'UPDATED_NEW',
         };
     }
 
